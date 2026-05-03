0229fbdc8b465f5b82559a659c7ae56c8229e563
mtegarseptian <mtegarseptian@gmail.com>
2026-05-03 21:05:57 +0700

    Fitur: membuat migrasi database untuk satelit dan stasiun bumi

---
+++ database/migrations/2026_05_03_135954_create_satellites_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('satellites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('country');
+            $table->date('launch_date');
+            $table->enum('orbit_type', ['LEO', 'MEO', 'GEO']);
+            $table->text('tle')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->foreignId('ground_station_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('satellites');
+    }
+};
